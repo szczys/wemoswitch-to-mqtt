c4e7ffc91c8ad8a449c3a37c92d6bf6d0136bdd7
Mike Szczys <szczys@hotmail.com>
2019-12-08 16:43:24 -0600

    Added scheduling but currently coded and not user changeable

--- wemo-mqtt-control.py
+++ wemo-mqtt-control.py
@@ -3,20 +3,33 @@
 #Install dependancy:
 #sudo pip3 install pywemo
 #sudo pip3 install paho-mqtt
+#sudo pip3 install schedule
 
 import pywemo
 import paho.mqtt.client as mqtt
+import schedule
+import time
+import datetime
 
 porchlight_addr = "192.168.1.137"
 topic = "lighting/porchlight"
 
 
+#Wemo functions
 
 #Don't use discovery, count on IP addresses
+#FIXME: What happens if switch is not found?
 port = pywemo.ouimeaux_device.probe_wemo(porchlight_addr)
 url = 'http://%s:%i/setup.xml' % (porchlight_addr, port)
 porchlight = pywemo.discovery.device_from_description(url, None)
 
+def wemo_changestate(client,state):
+    if state=="On":
+        porchlight.on()
+        publish_status(client)
+    elif state=="Off":
+        porchlight.off()
+        publish_status(client)
 
 #MQTT functions
 def publish_status(client):
@@ -37,20 +50,43 @@ def on_message(client, userdata, msg):
     except:
         return
 
-    if (cmd == "On"):
-        print("Setting state to: On")
-        porchlight.on()
-        publish_status(client)
-    elif (cmd == "Off"):
-        print("Setting state to: Off")
-        porchlight.off()
-        publish_status(client)
+    if (cmd == "On") or (cmd == "Off"):
+        print("Setting state to: %s" % cmd)
+        wemo_changestate(client,cmd)
     elif (cmd == "Status"):
         publish_status(client)
 
+#Scheduled task functions
+def service_sundown(client):
+    #Update any schedules with "sundown" in them to match daily changes
+    #this should be run every day mid-day
+    print("sundown schedule running")
+    newhour = 19
+    newminute = 21
+    sundown_events = [x for x in schedule.jobs if "sundown" in x.tags]
+    for event in sundown_events:
+        print(event)
+        n = event.next_run
+        event.next_run = datetime.datetime(n.year, n.month, n.day, newhour, newminute)
+        event.at_time = datetime.time(newhour, newminute)
+        client.publish(topic, payload="Next sundown set for: %s" % event.next_run.strftime("%H:%M"))
 
+#Setup MQTT Client
 client = mqtt.Client()
 client.connect("RaspberryPi",1883,60)
 client.on_connect = on_connect
 client.on_message = on_message
-client.loop_forever()
+#Start the MQTT thread that handles this client
+client.loop_start()
+
+#Do the scheduleing
+#FIXME: This should be user setable
+schedule.every().day.at("16:30").do(porchlight.on).tag("sundown")
+schedule.every().day.at("23:00").do(porchlight.off)
+#Schedule the sundown time updater
+schedule.every().day.at("16:42").do(service_sundown,client).tag("sundownscheduler")
+
+#Use 1 Hz loop to handle scheduling
+while(True):
+    schedule.run_pending()
+    time.sleep(1)
